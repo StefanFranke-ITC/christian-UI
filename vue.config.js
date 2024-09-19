17f858abf2e18132015cf71a52fd91773674955d
Leandro <leandro.graf@itc-studenten.de>
2024-09-19 23:30:13 +0200

    config js

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,17 @@
 module.exports = {
     transpileDependencies: [
-        // Hier kannst du Abhängigkeiten angeben, die transpiliert werden sollen, z.B. 'vuetify'.
-        // Wenn keine nötig sind, kann es leer bleiben.
         'vuetify'
     ],
 
     devServer: {
-        port: 3000,  // Port auf 3000 setzen
-        host: '0.0.0.0',  // Server lauscht auf allen Netzwerkschnittstellen, um auch von außen erreichbar zu sein
-        disableHostCheck: true,  // Optional: Host-Überprüfung deaktivieren, um externe Verbindungen zuzulassen
+        port: 3000,
+        host: '0.0.0.0',  // Damit der Server auf allen Schnittstellen lauscht
+        allowedHosts: 'all',  // Erlaubt Verbindungen von allen Hosts
     },
 
     pluginOptions: {
         vuetify: {
-            // Hier kannst du zusätzliche Vuetify-spezifische Optionen hinzufügen
-            // Das bleibt leer, wenn du keine speziellen Anpassungen vornehmen möchtest
+            // Vuetify-spezifische Konfigurationen können hier hinzugefügt werden
         }
     }
 };
